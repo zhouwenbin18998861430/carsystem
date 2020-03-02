2bb18cf36eccf14e0517d49649d5210c9519253e
zhouwenbin <1426392099@qq.com>
2020-03-02 23:32:56 +0800

    客户端登陆完成

--- src/main/java/com/zhouwenbin/carsystem/controller/LoginControl.java
+++ src/main/java/com/zhouwenbin/carsystem/controller/LoginControl.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +19,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/login")
-public class LoginControl {
+public class LoginControl extends HttpServlet {
     //    写控制方法
     @Autowired
     private UsersService usersService;
@@ -29,50 +32,65 @@ public class LoginControl {
     }
 //==============================================================================
     //登录校验
-    @GetMapping
-    public ModelAndView login(Model model) {
-        Users users = new Users();
-        model.addAttribute("users", users);
-        ModelAndView modelAndView = new ModelAndView("login", "userModel", model);
-        return modelAndView;
-    }
+    // @GetMapping
+    // public ModelAndView login(Model model) {
+    //     Users users = new Users();
+    //     model.addAttribute("users", users);
+    //     ModelAndView modelAndView = new ModelAndView("login", "userModel", model);
+    //     return modelAndView;
+    // }
 
     @PostMapping("/submit")
-    public String login(@ModelAttribute(value = "users") Users users, Model model) {
-        Integer loginType = users.getGrade();
-        String account = users.getAccount();
-        System.out.println("输入的用户：" + account);
-        String password = users.getPassword();
-        System.out.println("输入的密码：" + password);
-        String password1 = usersService.findPwd(account);
-        System.out.println("查询的密码：" + password1);
-        Integer grade = usersService.selectGrade(account);
-        if (password == null) {
-            model.addAttribute("msg","请先输入用户名或密码");
-            return "login";//没有输入密码
+    public String login( Model model, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session=request.getSession();//创建session对象
+        Users user=(Users) session.getAttribute("user");//保存session信息
+        if (user!=null){
+            System.out.println("该用户已经登录");
         }
-        if (password.equals(password1)) {
-            System.out.println("grade:" + grade);
+        // System.out.println("该用户尚未登录");
+        String account = request.getParameter("account");
+        // System.out.println("输入的用户：" + account);
+        String password = request.getParameter("password");
+        // System.out.println("输入的密码：" + password);
+        String password1 = usersService.findPwd(account);
+        // System.out.println("查询的密码：" + password1);
+        Integer grade = usersService.selectGrade(account);
+        // System.out.println("权限等级为:"+grade);
+        if (password.length() == 0) {
+            model.addAttribute("msg", "请先输入用户名或密码");
+            return "/login";//没有输入密码
+        } else if (password.equals(password1)) {
+            // System.out.println("grade:" + grade);
+            response.setContentType("text/html;charset=utf-8");//解决乱码
+            Users users = new Users();
+            users.setAccount(account);
+            users.setPassword(password1);
+            // HttpSession session = request.getSession();
             switch (grade) {
                 case 1:
 //        权限为1是管理员
-                    model.addAttribute("accountinfo", users);
+//                     model.addAttribute("accountinfo", users);
+                    session.setAttribute("user", users);//创建session保存对象
+                    // HttpSession session=request.getSession();//创建session保存信息
 //        ModelAndView modelAndView=new ModelAndView("index","usersModel",users);
-                    return "index1";
+                    return "redirect:/index1";
                 case 2:
 //        权限为2是租赁点管理员
-                    model.addAttribute("accountinfo", users);
+//                     model.addAttribute("accountinfo", users);
+                    session.setAttribute("user", users);//创建session保存对象
 //        ModelAndView modelAndView=new ModelAndView("index","usersModel",users);
-                    return "index2";
+                    return "redirect:/index2";
                 case 3:
 //        权限为3是普通用户
-                    model.addAttribute("accountinfo", users);
+//                     model.addAttribute("accountinfo", users);
+                    session.setAttribute("user", users);//创建session保存对象
 //        ModelAndView modelAndView=new ModelAndView("index","usersModel",users);
-                    return "index3";
+                    return "redirect:/index";
             }
-        }
-        model.addAttribute("msg", "用户名或密码错误 ");
-        return "login";//密码错了
+        } else
+            model.addAttribute("msg", "用户名或密码错误 ");
+        return "redirect:/login";//密码错了
+
     }
 
     //============================================================================
@@ -94,6 +112,8 @@ public class LoginControl {
             }
         return map;
     }
+//==============================================================================
+
 //==============================================================================
     @RequestMapping("/forgetpwd")
     @ResponseBody
--- src/main/java/com/zhouwenbin/carsystem/controller/PageControl.java
+++ src/main/java/com/zhouwenbin/carsystem/controller/PageControl.java
@@ -1,13 +1,82 @@
 package com.zhouwenbin.carsystem.controller;
 
+import com.zhouwenbin.carsystem.entity.Users;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
-public class PageControl {
+public class PageControl extends HttpServlet {
     @RequestMapping("/{page}")
     public String page(@PathVariable String page) {
         return page;
+    }
+    // @RequestMapping("/index1")
+    // public String index1(HttpServletRequest request, HttpServletResponse response,Model model) {
+    //     response.setContentType("text/html;charset=utf-8");//解决乱码
+    //     HttpSession session=request.getSession();//创建session对象
+    //     Users user=(Users) session.getAttribute("user");//保存session信息
+    //     if (user==null){
+    //         System.out.println("你还未登陆");
+    //         return "redirect:/login";
+    //     }else {
+    //         System.out.println("已登陆");
+    //         model.addAttribute("user",user);
+    //         // //创建Cookie对象用来保存Session的id
+    //         // Cookie cookie=new Cookie("JSESSIONID",session.getId());
+    //         // cookie.setMaxAge(30*60);
+    //         // cookie.setPath("/Project01");
+    //         // response.addCookie(cookie);
+    //         // model.addAttribute("user",user);
+    //     }
+    //     return "index1";
+    // }
+    // @RequestMapping("/index2")
+    // public String index2(HttpServletRequest request, HttpServletResponse response,Model model) {
+    //     response.setContentType("text/html;charset=utf-8");//解决乱码
+    //     HttpSession session=request.getSession();//创建session对象
+    //     Users user=(Users) session.getAttribute("user");//保存session信息
+    //     if (user==null){
+    //         System.out.println("你还未登陆");
+    //         return "redirect:/login";
+    //     }else {
+    //         System.out.println("已登陆");
+    //         model.addAttribute("user",user);
+    //         // //创建Cookie对象用来保存Session的id
+    //         // Cookie cookie=new Cookie("JSESSIONID",session.getId());
+    //         // cookie.setMaxAge(30*60);
+    //         // cookie.setPath("/Project02");
+    //         // response.addCookie(cookie);
+    //         // model.addAttribute("user",user);
+    //     }
+    //     return "index2";
+    // }
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request, HttpServletResponse response, Model model) {
+        response.setContentType("text/html;charset=utf-8");//解决乱码
+        HttpSession session=request.getSession();//创建session对象
+        Users user=(Users) session.getAttribute("user");//保存session信息
+        if (user==null){
+            System.out.println("你还未登陆");
+            model.addAttribute("msg", "你尚未登录");
+            // return "redirect:/login";
+        }else {
+            System.out.println("已登陆");
+            model.addAttribute("user",user);
+            // System.out.println("user1"+user);
+            // //创建Cookie对象用来保存Session的id
+            // Cookie cookie=new Cookie("JSESSIONID",session.getId());
+            // cookie.setMaxAge(30*60);
+            // cookie.setPath("/Project0");
+            // response.addCookie(cookie);
+            // model.addAttribute("user",user);
+        }
+        return "index";
     }
 }
